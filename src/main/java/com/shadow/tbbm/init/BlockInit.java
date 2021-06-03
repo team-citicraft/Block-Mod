140ffa15a2db658ac89138c5a12a79fb67fd40a2
BuildTools <unconfigured@null.spigotmc.org>
2021-06-03 19:37:43 -0400

    started adding recipes.

--- src/main/java/com/shadow/tbbm/init/BlockInit.java
+++ src/main/java/com/shadow/tbbm/init/BlockInit.java
@@ -14,5 +14,5 @@ public class BlockInit {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS,
             BlocksMod.MOD_ID);
 
-    public static final RegistryObject<RotBlock> H_S_W = BLOCKS.register("h_s_w", () -> new RotBlock(AbstractBlock.Properties.from(Blocks.OAK_PLANKS)));
+    public static final RegistryObject<Block> H_S_W = BLOCKS.register("h_s_w", () -> new Block(AbstractBlock.Properties.from(Blocks.OAK_PLANKS)));
 }
--- src/main/java/com/shadow/tbbm/init/ItemInit.java
+++ src/main/java/com/shadow/tbbm/init/ItemInit.java
@@ -2,9 +2,13 @@ package com.shadow.tbbm.init;
 
 import com.shadow.tbbm.BlocksMod;
 import net.minecraft.item.Item;
+import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
 public class ItemInit {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, BlocksMod.MOD_ID);
+
+    public static final RegistryObject<Item> PANEL = ITEMS.register("panel",
+            () -> new Item(new Item.Properties().group(BlocksMod.TAB)));
 }
