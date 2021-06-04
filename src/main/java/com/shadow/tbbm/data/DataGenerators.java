6c24783fd8b60c129c8b3cae5a807a1c84bd725c
Yusuf Ismail <yusufgamer222@gmail.com>
2021-06-04 18:33:25 +0100

    creating datafenerator for models first

---
+++ src/main/java/com/shadow/tbbm/data/DataGenerators.java
@@ -0,0 +1,19 @@
+import com.shadow.tbbm.BlocksMod;
+import net.minecraft.data.DataGenerator;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.GatherDataEvent;
+
+@Mod.EventBusSubscriber(modid = BlocksMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public final class DataGenerators {
+    private DataGenerators() {
+    }
+
+
+    @SubscribeEvent
+    public static void gatherData(GatherDataEvent event) {
+        DataGenerator gen = event.getGenerator();
+        ExistingFileHelper existingFileHelper = event.getExistingFileHelper();
+    }
+}
