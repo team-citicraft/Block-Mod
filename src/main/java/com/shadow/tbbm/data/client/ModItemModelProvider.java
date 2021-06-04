ef590eeec5fae958fce87fcc251b726e1b6c2da2
Yusuf Ismail <yusufgamer222@gmail.com>
2021-06-04 20:51:09 +0100

    removed models from normal folder

--- src/main/java/com/shadow/tbbm/data/client/ModItemModelProvider.java
+++ src/main/java/com/shadow/tbbm/data/client/ModItemModelProvider.java
@@ -2,7 +2,9 @@ package com.shadow.tbbm.data.client;
 
 import com.shadow.tbbm.BlocksMod;
 import net.minecraft.data.DataGenerator;
+import net.minecraftforge.client.model.generators.ItemModelBuilder;
 import net.minecraftforge.client.model.generators.ItemModelProvider;
+import net.minecraftforge.client.model.generators.ModelFile;
 import net.minecraftforge.common.data.ExistingFileHelper;
 
 public class ModItemModelProvider extends ItemModelProvider {
@@ -16,12 +18,30 @@ public class ModItemModelProvider extends ItemModelProvider {
 
     @Override
     protected void registerModels() {
-    //here you will do the models for the blocks and items.
+        //here you will do the models for the blocks and items.
         withExistingParent("h_s_w", modLoc("block/h_s_w"));//the first one will be the name of the file.
         //the second will be location. For vanila datagenertor the location have to be block not blocks its stupid.
         //Same applies to items
 
+        ModelFile itemGenerated = getExistingFile(mcLoc("item/generated")); //for normal items
+        ModelFile itemhandHeld = getExistingFile(mcLoc("item/handheld")); //for handheld items such as a swords
+
+        //items
+        builder(itemGenerated, "panel");
+        builder(itemGenerated, "saw");
 
 
+        //tools
+        //example from my mod for a tool
+        // tool(itemhandHeld, "heatblast_sword");
     }
+        private ItemModelBuilder builder(ModelFile itemGenerated, String name) {
+            return getBuilder(name).parent(itemGenerated).texture("layer0", "item/" + name);
+        }
+        private ItemModelBuilder tool(ModelFile itemhandHeld, String name) {
+            return getBuilder(name).parent(itemhandHeld).texture("layer0",  "item/" + name);
+        }
+
+
+
 }
